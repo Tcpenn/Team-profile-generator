eda16ece9d10166bef99a65c2983ccb767fa9610
tcpenn <tcpenn1026@gmail.com>
2021-10-04 00:09:32 -0400

    finished generate page logic

--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -1,17 +1,16 @@
-const { expect } = require('@jest/globals');
 const Manager = require('../lib/Manager');
 
 test('creates a manager object', () => {
-    const manager = new Manager('john', 1256, 'john@team.com', 1) 
+  const manager = new Manager('Karen', 1234, 'karen@company.com', 2);
 
-    expect(manager.name).toBe('john');
-    expect(manager.id).toBe(1256);
-    expect(manager.email).toBe('john@team.com')
-    expect(manager.officeNumber).toBe(1);
+  expect(manager.name).toBe('Karen');
+  expect(manager.id).toBe(1234);
+  expect(manager.email).toBe('karen@company.com');
+  expect(manager.officeNumber).toBe(2);
 });
 
-test ('returns the managers role', () => {
-    const manager = new Manager('john,', 1256, 'john@team.com', 1);
+test("returns manager's role", () => {
+  const manager = new Manager('Karen', 1234, 'karen@company.com', 2);
 
-    expect(manager.getRole()).toBe('Manager');
+  expect(manager.getRole()).toBe('Manager');
 });
